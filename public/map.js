e6b592c24a82fa1dcf56443b8614275cc044bd8c
Nhat Trinh <goodyshoes_nt@yahoo.com>
2018-04-29 07:34:41 -0700

    display finished

--- public/map.js
+++ public/map.js
@@ -1,11 +1,41 @@
+var users={
+  1:{
+    lat: 36.5486997,
+    long: -122.0589753
+  },
+  2:{
+    lat: 37.5486997,
+    long: -123.0589753
+  }
+};
+
+function addUser(user, name){
+  if (user){
+    users[user] = 
+  }
+}
+
 function myMap() {
     var mapCanvas = document.getElementById("map");
     var myCenter = new google.maps.LatLng(37.5486997,-122.0589753); 
+    
+    // initialize all the markers with the user objects
+    var centers = [];
+    var markers = [];
+    Object.keys(users).map((v,i)=>{
+      centers.push(new google.maps.LatLng(users[v].lat,users[v].long));
+      markers.push(new google.maps.Marker({position : centers[i],animation: google.maps.Animation.BOUNCE}));
+    });
+    
     var mapOptions = {center: myCenter, zoom: 12};
     var map = new google.maps.Map(mapCanvas,mapOptions);
     var marker = new google.maps.Marker({
       position: myCenter,
       animation: google.maps.Animation.BOUNCE
     });
-    marker.setMap(map);
+
+    markers.map((v,i)=>{
+      v.setMap(map);
+    });
+    //marker.setMap(map);
   }
