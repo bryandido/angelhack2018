30faa7bb167abc9641b47cd090de2eda213fe86b
Shafi Dayatar <shafi.dayatar@gmail.com>
2018-04-29 07:16:11 -0700

    client update

--- public/socket.js
+++ public/socket.js
@@ -5,14 +5,16 @@ $(function () {
         $('#messages').text(msg);
         name = msg["profile"]["name"]
         if (data[name] === undefined){
-            $('#list').append('<tr id="' + name + '<td>' + msg["time"] +"</td><td>" + name + "</td><td>" + msg["message"] + "</td></tr>")
+            $('#list').append('<tr id="' + name + '"><td>' 
+                + msg["time"] +"</td><td>" + name + "</td><td>" + msg["mes"] + "</td></tr>")
             data[name] = {}
         }else{
             data[name] = {
                 time: msg["profile"]["time"],
                 location: msg["profile"]["location"]
                 }
-            $('#'+name).html('<td>' + msg["time"] +"</td><td>" + name + "</td><td>" + msg["message"] + "</td>")
+            $('#'+name).html('<td>' + msg["time"] +"</td><td>" + name + "</td><td>" + msg["mes"] + "</td>")
+        }
     });
     socket.on('admin_ip', function (msg) {
         $('#ip').text(msg);
